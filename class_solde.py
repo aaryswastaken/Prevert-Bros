c39c3299b78923630b78be7c126c8255bddb10ae
Heleneeeeeeee <169167216+Heleneeeeeeee@users.noreply.github.com>
2024-05-29 15:23:19 +0200

    Update class_solde.py

--- class_solde.py
+++ class_solde.py
@@ -1,34 +1,26 @@
 import tkinter as tk
 
-class Solde:
-    """
-    Classe qui permet le décompte du temps / solde restant au joueur pour terminer le niveau
+'''
+il faut utiliser after et non sleep '''
 
-    Attributs : solde_initial (int)
-    """
-    __slots__=['solde_initial']
+class Solde:
+    __slots__=['solde_initial', 'solde_restant']
     
     def __init__(self):
         self.solde_initial = 200
-
+        self.solde_restant=0
     
     def decompte(self):
-        """
-        Décrementation du solde de 1, lorsqu'il atteint 0, afficher Game Over 
-        """
         self.solde_initial -=1
         if self.solde_initial ==0:
              print("GAME OVER !")
+        
+        #RAJOUTER LE FAIT QUE SI PLAYER TERMINE LE NIVEAU AVANT LEPUISEMENT DU SOLDE ALORS ON ENREGISTRE LE SOLDE EN SOLDE RESTANT
+        # return solde_restant
+    
 
-#à mettre dans l'affichage 
+
 class Solde_af(tk.Tk):
-    """
-    Classe pour afficher le solde dans une interface Tkinter.
-
-    Attribute:
-        solde : Instance de la classe Solde.
-        label_solde 
-    """
     def __init__(self, solde):
         super().__init__()
         self.solde=solde
@@ -38,18 +30,15 @@ class Solde_af(tk.Tk):
         
         
     def afficher(self):
-        """
-        Met à jour l'affichage du solde qui diminue toutes les 1,5 sec, lorsque le solde affiche 0, "Game Over" apparaît
-        """
         self.solde.decompte()
         self.label_solde['text'] = f"Solde : {self.solde.solde_initial}"
         self.label_solde.pack()
         if self.solde.solde_initial >0:
-            self.after(1500, self.afficher)
+            self.after(1000, self.afficher)
         else:
             self.label_solde =tk.Label(self, text= "GAME OVER !!!!!")
             self.label_solde.pack()
-            #self.after(1500, self.destroy())
+            self.after(1500, self.destroy())
       
     
 f = Solde_af(Solde())
