1c3008bf7efe21b8049095575588d3c9eea4d1b2
AntoFarfadet <antoine.demin@insa-lyon.fr>
2024-05-29 15:59:57 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -17,24 +17,25 @@ if __name__ == "__main__":
         print("Debugging")
         debugFlag = True
 
-    manager = GameManager(debug=debugFlag)  # Create a new manager
+    manager = GameManager(debug=debugFlag)
 
     ## Debug scene
     from object import RectGroundPart
     from player import Player
     from common import V2
-
-
-    # Add two rectangles
-    manager.addObject(RectGroundPart(V2(-500, 0), V2(2000, 200), "#00ff00")) 
-    manager.addObject(RectGroundPart(V2(700, 200), V2(300, 50), "#00ffff"))
-    # manager.addObject(RectGroundPart(V2(800, 300), V2(200, 200), "#00ffff"))
+    from niveaux import Niveaux
     
+    for platform in Niveaux().coord:
+        manager.addObject(RectGroundPart(V2(platform[0], platform[1]), V2(platform[2], platform[3]), "#00ff00"))
+    
+    # manager.addObject(RectGroundPart(V2(-500, 0), V2(2000, 200), "#00ff00"))
+    # manager.addObject(RectGroundPart(V2(700, 200), V2(300, 50), "#00ffff"))
+    # manager.addObject(RectGroundPart(V2(800, 300), V2(200, 200), "#00ffff"))
     player = Player()
     player.pos = V2(50, 350)
-    manager.addObject(player, hasInput=True, isPlayer=True) # register the player
+    manager.addObject(player, hasInput=True, isPlayer=True)
 
-    ec = manager.run() # run the game
+    ec = manager.run()
 
     if ec != 0:
         print("An error has occured, please check logs")
---
+++ niveaux.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 29 14:54:08 2024
+
+@author: ademin
+"""
+
+class Niveaux():
+    
+    __slots__ = ['x', 'y', 'l', 'h', 'nv1', 'coord']
+    
+    def __init__(self):
+        self.x = [-500, 400, 600, 700, 800, 1100, 1450, 1600, 1750, 1900]
+        self.y = 0
+        self.l = [800, 200, 100, 100, 100, 250, 50, 50, 50, 400]
+        self.h = [200, 200, 270, 340, 411, 200, 200, 200, 200, 200]
+        self.create_dic()
+        
+    def create_dic(self):
+        self.nv1 = dict()
+        if len(self.x) == len(self.l) and len(self.l) == len(self.h):
+            for i in range(len(self.x)):
+                self.nv1[str(i)] = (self.x[i], self.y, self.l[i], self.h[i])
+            self.coord = self.nv1.values()
+        else:
+            print("The number of levels is not coherent")
