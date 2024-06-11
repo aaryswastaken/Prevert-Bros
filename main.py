0b864a83c373243e88a568f7313b04be87b93bca
Heleneeeeeeee <169167216+Heleneeeeeeee@users.noreply.github.com>
2024-06-11 13:00:52 +0200

    Update main.py
    modification du design du perso

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     # manager.addObject(RectGroundPart(V2(-500, 0), V2(2000, 200), "#00ff00"))
     # manager.addObject(RectGroundPart(V2(700, 200), V2(300, 50), "#00ffff"))
     # manager.addObject(RectGroundPart(V2(800, 300), V2(200, 200), "#00ffff"))
-    player = Player()
+    player = Player(image="./PERSO.png")
     player.pos = V2(50, 350)
     manager.addObject(player, hasInput=True, isPlayer=True)
 
