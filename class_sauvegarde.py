8976e718feac9733465df5786468e077944a5ed7
AntoFarfadet <antoine.demin@insa-lyon.fr>
2024-06-12 19:25:27 +0200

    Add files via upload

--- class_sauvegarde.py
+++ class_sauvegarde.py
@@ -1,39 +1,43 @@
 import csv
-from class_solde import Solde
-from class_pieces import Pieces
+import os
 
-#/!\ CLASSE ISSUE DE SOLDE ET DE PIECES
+class Sauvegardes:
+    def __init__(self):        
+        self.nom_fichier = 'sauvegardes.csv'
+        
+        #Vérifie si le fichier existe
+        if not os.path.isfile(self.nom_fichier):
+            # Crée le fichier et écrit les en-têtes
+            with open(self.nom_fichier, mode='w', newline='') as fichier_csv:
+                writer = csv.writer(fichier_csv)
+                writer.writerow(['Cookies Recoltees', 'Solde Restant'])
+        
+        self.getRecords()
 
-class Sauvegarde(Solde, Pieces):
-    """
-    Classe sauvegarde qui permet d'enregistrer dans un fichier csv, le solde restant et le nombre de pièces récoltées par le joueur à la fin du jeu
-    """
-    __slots__ = ['nom_fichier']
+    def save(self, pieces_recoltees, solde_restant):
+        # Ouvre le fichier en mode ajout ('a')
+        with open(self.nom_fichier, mode='a', newline='') as fichier_csv:
+            writer = csv.writer(fichier_csv)
+            
+            # Ajoute les nouvelles données
+            writer.writerow([pieces_recoltees, solde_restant])
     
-    def __init__(self, nom_fichier):
-        super().__init__()
-        self.nom_fichier = nom_fichier
-        self.solde =Solde.solde_restant
-        self.pieces= Pieces.compteur
-        
-    def creation_fichier(self):
-        """
-        créer un fichier csv avec la première ligne vide 
-        """
-        with open(nom_fichier,'w') as fichier_sauvegarde:
-            writer = csv.writer(fichier_sauvegarde)
-            writer.writerow([])
-
-#à lancer dans un autre fichier associé à la fin du jeu
-    def enregistrement_sauvegarde(self):
-        """
-        enregistre le score et le nombre de pièces dans le fichier
-        """
-        with open(nom_fichier,'w') as fichier_sauvegarde:
-            writer = csv.writer(fichier_sauvegarde)
-            writer.writerow([self.solde, self.pieces])
-        
-        
-        
-
-nom_fichier= 'sauvegarde.tkt'
+    def getRecords(self):
+        #Ouvre le fichier en mode lecture
+        with open(self.nom_fichier, mode='r', newline='') as fichier_csv:
+            reader = csv.DictReader(fichier_csv)
+            
+            max_pieces_recoltees = 0
+            min_solde_restant = 360
+            
+            for row in reader:
+                pieces_recoltees = int(row['Cookies Recoltees'])
+                solde_restant = int(row['Solde Restant'])
+                
+                if pieces_recoltees > max_pieces_recoltees:
+                    max_pieces_recoltees = pieces_recoltees
+                
+                if solde_restant < min_solde_restant:
+                    min_solde_restant = solde_restant
+            
+            self.cookiesRecord, self.soldeRecord = max_pieces_recoltees, min_solde_restant
